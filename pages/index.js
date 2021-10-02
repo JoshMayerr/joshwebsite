8212918ec1da35c7ed5579e12f4fb6d251f25908
Josh Mayer <josh.25.mayer@gmail.com>
2021-10-02 08:37:50 -0700

    transition

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,49 @@
 import Nav from "../components/Nav";
 import Link from "next/link";
+import FadeIn from "react-fade-in";
 
 export default function Home() {
   return (
     <Nav activeLink={"Home"}>
       <div className="text-left flex flex-col justify-center min-h-screen pb-12 -my-28 sm:-my-24">
-        <h1 className="font-body text-9xl pt-10 sm:pt-0 sm:text-9xl tracking-wide  ">
-          Josh Mayer
-        </h1>
-        <div className="tracking-tight">
-          <p className="sm:text-xl text-lg leading-6 mt-4 lg:w-1/2">
-            17. San Francisco. Mostly a student. Occasionally a{" "}
-            <span className="hover:blur-sm transition ease-in-out filter animate-pulse">
-              <Link href="/worklog">
-                <a className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-green-500 ">
-                  programmer
+        <FadeIn delay={200} transitionDuration={500}>
+          <h1 className="font-body text-9xl pt-10 sm:pt-0 sm:text-9xl tracking-wide font-medium">
+            Josh Mayer
+          </h1>
+          <div className="tracking-tight">
+            <p className="sm:text-xl text-lg leading-6 mt-4 lg:w-1/2">
+              17. San Francisco. Mostly a student. Occasionally a{" "}
+              <span className="hover:blur-sm transition ease-in-out filter animate-pulse">
+                <Link href="/worklog">
+                  <a className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-green-500 ">
+                    programmer
+                  </a>
+                </Link>
+              </span>
+              ,{" "}
+              <span className="hover:blur-sm transition ease-in-out filter animate-pulse">
+                <a
+                  href="https://www.behance.net/jooshmayer"
+                  target="_blank"
+                  className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-indigo-500 "
+                >
+                  designer
                 </a>
-              </Link>
-            </span>
-            ,{" "}
-            <span className="hover:blur-sm transition ease-in-out filter animate-pulse">
-              <a
-                href="https://www.behance.net/jooshmayer"
-                target="_blank"
-                className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-indigo-500 "
-              >
-                designer
-              </a>
-            </span>
-            , and{" "}
-            <span className="hover:blur-sm transition ease-in-out filter animate-pulse">
-              <a
-                href="https://www.behance.net/jooshmayer"
-                target="_blank"
-                className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-red-500 "
-              >
-                creator
-              </a>
-            </span>
-            .
-          </p>
-        </div>
+              </span>
+              , and{" "}
+              <span className="hover:blur-sm transition ease-in-out filter animate-pulse">
+                <a
+                  href="https://www.behance.net/jooshmayer"
+                  target="_blank"
+                  className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-red-500 "
+                >
+                  creator
+                </a>
+              </span>
+              .
+            </p>
+          </div>
+        </FadeIn>
       </div>
     </Nav>
   );
--- pages/worklog.js
+++ pages/worklog.js
@@ -5,6 +5,7 @@ import Project from "../components/Project";
 import { Disclosure, Transition } from "@headlessui/react";
 import AddTag from "../components/AddTag";
 import { useState, useEffect } from "react";
+import FadeIn from "react-fade-in";
 
 const tags = [
   "laravel",
@@ -40,45 +41,48 @@ export default function Worklog({ projects = [] }) {
 
   return (
     <Nav activeLink={"My Worklog"}>
-      <div className="flex flex-col items-center mb-24">
-        <h1 className="font-body text-8xl tracking-wide text-center mt-4 sm:mt-8">
-          My Worklog
-        </h1>
-        <p className="mt-4 sm:mt-6 sm:w-3/4 md:w-1/2 w-full text-lg justify-self-center text-center ">
-          All of my biggest projects, ever made, all in one place. For the rest
-          of my design work, check out my{" "}
-          <a
-            className="hover:underline"
-            href="https://www.behance.net/jooshmayer"
-            target="_blank"
-          >
-            Behance.
-          </a>
-        </p>
-        <div className="mt-4 sm:mt-6 sm:w-3/4 w-full  ">
-          <div>
-            <div className="mt-1 flex rounded-md shadow-sm">
-              <div className="relative flex items-stretch flex-grow focus-within:z-10">
-                <input
-                  type="text"
-                  name="filter"
-                  id="filter"
-                  className="focus:ring-indigo-500 sm:text-md focus:border-indigo-500 block w-full rounded-none tracking-wider py-3 px-4 rounded-l-md border-2 border-r-0  border-black placeholder-black dark:bg-transparent dark:placeholder-white dark:border-white"
-                  placeholder="Search to Filter..."
-                  onKeyUp={(e) => setFilter(e.target.value)}
-                  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                />
-              </div>
-              <button
-                className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border-l-0 border-2 border-gray-900 dark:border-white text-sm font-medium rounded-r-md dark:bg-white text-white dark:text-black bg-black  focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 "
-                onClick={() => setFilter(filter)}
-                // !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      <FadeIn>
+        <div className="flex flex-col items-center mb-24">
+          <h1 className="font-body text-8xl tracking-wide text-center mt-4 sm:mt-8 font-medium">
+            My Worklog
+          </h1>
+          <div className="mt-4 sm:mt-6 sm:w-3/4 md:w-1/2 w-full justify-self-center text-center">
+            <p className=" text-lg">
+              All of my biggest projects, ever made, all in one place. For the
+              rest of my design work, check out my{" "}
+              <a
+                className="hover:underline"
+                href="https://www.behance.net/jooshmayer"
+                target="_blank"
               >
-                <ArrowSmRightIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-            </div>
+                Behance.
+              </a>
+            </p>
+          </div>
+          <div className="mt-4 sm:mt-6 sm:w-3/4 w-full  ">
+            <div>
+              <div className="mt-1 flex rounded-md shadow-sm">
+                <div className="relative flex items-stretch flex-grow focus-within:z-10">
+                  <input
+                    type="text"
+                    name="filter"
+                    id="filter"
+                    className="focus:ring-indigo-500 sm:text-md focus:border-indigo-500 block w-full rounded-none tracking-wider py-3 px-4 rounded-l-md border-2 border-r-0  border-black placeholder-black dark:bg-transparent dark:placeholder-white dark:border-white"
+                    placeholder="Search to Filter..."
+                    onKeyUp={(e) => setFilter(e.target.value)}
+                    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                  />
+                </div>
+                <button
+                  className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border-l-0 border-2 border-gray-900 dark:border-white text-sm font-medium rounded-r-md dark:bg-white text-white dark:text-black bg-black  focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 "
+                  onClick={() => setFilter(filter)}
+                  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                >
+                  <ArrowSmRightIcon className="h-5 w-5" aria-hidden="true" />
+                </button>
+              </div>
 
-            {/*            
+              {/*            
             <div className="mt-2">
               <Disclosure>
                 {({ open }) => (
@@ -123,35 +127,36 @@ export default function Worklog({ projects = [] }) {
                 )}
               </Disclosure>
             </div> */}
+            </div>
+          </div>
+
+          <div className="mt-10 w-full">
+            <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 ">
+              {allProjects.length > 0 ? (
+                allProjects.map((project) => (
+                  <Project
+                    key={project.sys.id}
+                    title={project.fields.title}
+                    blurb={project.fields.blurb}
+                    richText={project.fields.richText}
+                    thumbnail={project.fields.thumbnail.fields.file.url}
+                    photo={project.fields.photos[0].fields.file.url}
+                    githubLink={project.fields.githubLink}
+                    demoLink={project.fields.demoLink}
+                    behanceLink={project.fields.behanceLink}
+                    date={project.fields.date}
+                    tags={project.metadata.tags}
+                  />
+                ))
+              ) : (
+                <div className="text-white text-lg col-span-3 flex justify-center lg:h-48">
+                  No projects found with this criteria :)
+                </div>
+              )}
+            </ul>
           </div>
         </div>
-
-        <div className="mt-10 w-full">
-          <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 ">
-            {allProjects.length > 0 ? (
-              allProjects.map((project) => (
-                <Project
-                  key={project.sys.id}
-                  title={project.fields.title}
-                  blurb={project.fields.blurb}
-                  richText={project.fields.richText}
-                  thumbnail={project.fields.thumbnail.fields.file.url}
-                  photo={project.fields.photos[0].fields.file.url}
-                  githubLink={project.fields.githubLink}
-                  demoLink={project.fields.demoLink}
-                  behanceLink={project.fields.behanceLink}
-                  date={project.fields.date}
-                  tags={project.metadata.tags}
-                />
-              ))
-            ) : (
-              <div className="text-white text-lg col-span-3 flex justify-center lg:h-48">
-                No projects found with this criteria :)
-              </div>
-            )}
-          </ul>
-        </div>
-      </div>
+      </FadeIn>
     </Nav>
   );
 }
