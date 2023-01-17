c1402b031027d310bf00b203cb5905ad2f242af9
Josh Mayer <josh.25.mayer@gmail.com>
2023-01-16 20:08:05 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -65,7 +65,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:mt-0 md:order-1">
           <p className="text-center text-base ">
-            &copy; 2021 Mr. Mayer, Inc. All rights reserved.
+            &copy; 2023 Mr. Mayer, Inc. All rights reserved.
           </p>
         </div>
       </div>
